8a89cccd475f167e3d030abd79cfd8d1bb6c617c
Jared <jrussell@bmtx.com>
2022-07-12 14:33:38 -0400

    Added Item.cs properties.

--- Sample.Core/Models/Item.cs
+++ Sample.Core/Models/Item.cs
@@ -1,11 +1,12 @@
 namespace Sample.Core.Models
 {
-    public class Coffee
+    public class Item
     {
         //[PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Roaster { get; set; }
-        public string Name { get; set; }
         public string Image { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
     }
 }
