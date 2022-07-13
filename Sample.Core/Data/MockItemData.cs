21fb7bdc9d4eb5f59650b44436eb455551be868c
Jared <jrussell@bmtx.com>
2022-07-12 23:14:55 -0400

    MatchaBag.png added.

--- Sample.Core/Data/MockItemData.cs
+++ Sample.Core/Data/MockItemData.cs
@@ -11,6 +11,20 @@ namespace Sample.Core.Data
                 Image = "CoffeeBag",
                 Name = "Tasty Coffee",
                 Description = "The best coffee in town!",
+                Price = 15.99
+            },
+            new Item
+            {
+                Image = "MatchaBag",
+                Name = "Love Matcha Tea",
+                Description = "Finest grade organic japanese matcha.",
+                Price = 39.99
+            },
+            new Item
+            {
+                Image = "CoffeeBag",
+                Name = "Tasty Coffee",
+                Description = "The best coffee in town!",
                 Price = 1.99
             }
         };
