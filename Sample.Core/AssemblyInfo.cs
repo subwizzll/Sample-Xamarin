8772fab0138a85d1b844f2e907f90fb7011da02b
Jared <jrussell@bmtx.com>
2022-07-14 16:35:14 -0400

    Remove unnecessary fonts

--- Sample.Core/AssemblyInfo.cs
+++ Sample.Core/AssemblyInfo.cs
@@ -3,15 +3,9 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 // Fonts
-[assembly: ExportFont("Roboto-Black.ttf", Alias = "Roboto-Black")]
-[assembly: ExportFont("Roboto-BlackItalic.ttf", Alias = "Roboto-BlackItalic")]
 [assembly: ExportFont("Roboto-Bold.ttf", Alias = "Roboto-Bold")]
 [assembly: ExportFont("Roboto-BoldItalic.ttf", Alias = "Roboto-BoldItalic")]
 [assembly: ExportFont("Roboto-Medium.ttf", Alias = "Roboto-Medium")]
 [assembly: ExportFont("Roboto-MediumItalic.ttf", Alias = "Roboto-MediumItalic")]
 [assembly: ExportFont("Roboto-Regular.ttf", Alias = "Roboto-Regular")]
 [assembly: ExportFont("Roboto-Italic.ttf", Alias = "Roboto-Italic")]
-[assembly: ExportFont("Roboto-Light.ttf", Alias = "Roboto-Light")]
-[assembly: ExportFont("Roboto-LightItalic.ttf", Alias = "Roboto-LightItalic")]
-[assembly: ExportFont("Roboto-Thin.ttf", Alias = "Roboto-Thin")]
-[assembly: ExportFont("Roboto-ThinItalic.ttf", Alias = "Roboto-ThinItalic")]
