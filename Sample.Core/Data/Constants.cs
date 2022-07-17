0b58b639bbaf82dbec9b7428d3bb0a0196edb8cc
Jared <jrussell@bmtx.com>
2022-07-17 17:03:47 -0400

    Constants.cs change

--- Sample.Core/Data/Constants.cs
+++ Sample.Core/Data/Constants.cs
@@ -6,7 +6,7 @@ namespace Sample.Core.Data
     {
         public static class TaxRateApi
         {
-            public static Uri BaseAddress => new Uri("https://api.taxjar.com/");
+            public static Uri BaseAddress => new("https://api.taxjar.com/");
             public static string PublicKey => "5da2f821eee4035db4771edab942a4cc";
         }   
     }
