2508ac636780ec3eb08e8c12d2c735df303ddd8d
Jared <jrussell@bmtx.com>
2022-07-17 19:41:53 -0400

    IsNullOrEmptyOrZeroConverter.cs & IsNullOrEmptyOrZeroConverter added

---
+++ Sample.Core/Converters/IsNullOrEmptyOrZeroConverter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Globalization;
+using Xamarin.CommunityToolkit.Extensions.Internals;
+using Xamarin.Forms;
+
+namespace Sample.Core.Converters
+{
+    public class IsNullOrEmptyOrZeroConverter : ValueConverterExtension, IValueConverter
+    {
+        public virtual object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture) => IsNullOrEmptyOrZeroConverter.ConvertInternal(value);
+
+        internal static bool ConvertInternal(object? value)
+        {
+            if (value == null)
+                return true;
+            var test = value is string str && (string.IsNullOrWhiteSpace(str) || str == "0");
+            return test;
+        }
+        
+        public object ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+            => throw new NotImplementedException();
+    }    
+    
+    public class IsNotNullOrEmptyOrZeroConverter : IsNullOrEmptyOrZeroConverter
+    {
+        public override object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture) => IsNotNullOrEmptyOrZeroConverter.ConvertInternal(value);
+
+        static bool ConvertInternal(object? value)
+            => !IsNullOrEmptyOrZeroConverter.ConvertInternal(value);
+        
+        public object ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+            => throw new NotImplementedException();
+    }
+}
