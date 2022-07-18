118dfa40c8b121d9312e1fe9b9d5072b2e42342b
Jared <jrussell@bmtx.com>
2022-07-18 14:13:13 -0400

    CartService implemented in StorePage.xaml

--- Sample.Core/ViewModels/StoreViewModel.cs
+++ Sample.Core/ViewModels/StoreViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using MvvmCross.Commands;
-using MvvmCross.Navigation;
 using Sample.Core.Services;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using Sample.Core.Models;
 using Sample.Core.Models.TaxCalcStore;
-using Sample.Core.Service;
 
 namespace Sample.Core.ViewModels
 {
@@ -21,19 +16,16 @@ namespace Sample.Core.ViewModels
         IMvxAsyncCommand _myCartCommand;
         public IMvxAsyncCommand MyCartCommand => _myCartCommand ??= new MvxAsyncCommand(async () =>
         {
-            //await _navigationService.Navigate<MyCartViewModel>();
+            await _navigationService.Navigate<MyCartViewModel>();
         });
 
         IMvxAsyncCommand<(Item item, int qty)> _addToCartCommand;
         public IMvxAsyncCommand<(Item item, int qty)> AddToCartCommand => _addToCartCommand ??= new MvxAsyncCommand<(Item item, int qty)>(async parameter =>
         {
-            await Task.Run(() =>
-            {
-                for (var i = 0; i < parameter.qty; i++)
-                {
-                    _cartService.Cart.LineItems.Add(parameter.item);
-                }
-            });
+            if (parameter.qty > 1)
+                await _cartService.AddItems(parameter);
+            else
+                await _cartService.AddItem(parameter.item);
         });
     }
 }
