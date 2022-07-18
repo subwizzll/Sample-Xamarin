1db3f5ea120b1b9d19d0aee097ca4b9812e5199f
Jared <jrussell@bmtx.com>
2022-07-18 14:11:44 -0400

    CartService added

--- Sample.Core/Services/CartService.cs
+++ Sample.Core/Services/CartService.cs
@@ -1,6 +1,6 @@
 using System.Threading.Tasks;
-using Sample.Core.Framework;
 using Sample.Core.Models.TaxCalcStore;
+using static Sample.Core.Framework.Extensions;
 
 namespace Sample.Core.Services
 {
@@ -9,13 +9,28 @@ namespace Sample.Core.Services
         public Cart Cart { get; set; }
         public Task AddItem(Item item);
         public Task AddItems((Item item, int qty) value);
+        public Task RemoveItem(Item item);
+        public Task RemoveItems((Item item, int qty) value);
         public Task Reset();
     }
+
     public class CartService : ICartService
     {
         public Cart Cart { get; set; } = new();
-        public async Task AddItem(Item item) => Cart.LineItems.Add(item);
-        public async Task AddItems((Item item, int qty) value) => value.item.Repeated(value.item, value.qty);
-        public async Task Reset() => Cart.LineItems.Clear();
+        
+        public async Task AddItem(Item item)
+            => Cart.Items.Add(item);
+
+        public async Task AddItems((Item item, int qty) value)
+            => Cart.Items.AddRange(value.item.Repeated(value.qty));
+
+        public Task RemoveItem(Item item)
+            => throw new System.NotImplementedException();
+
+        public Task RemoveItems((Item item, int qty) value)
+            => throw new System.NotImplementedException();
+
+        public async Task Reset() 
+            => Cart.Items.Clear();
     }
 }
