823b82e43c149f5a8432f780f11ad1bae85710ac
Jared <jrussell@bmtx.com>
2022-07-20 17:12:12 -0400

    Address model

---
+++ Sample.Core/Models/TaxCalcStore/Address.cs
@@ -0,0 +1,13 @@
+namespace Sample.Core.Models.TaxCalcStore
+{
+    public class Address
+    {
+        public string City { get; set; }
+        
+        public string State { get; set; }
+        
+        public string Zip { get; set; }
+        
+        public string Country { get; set; }
+    }
+}
